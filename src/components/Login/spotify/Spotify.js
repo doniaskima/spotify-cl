7c4b0729232dff8c618047a5f26a0783c552a3a6
doniaskima <doniaskima344@gmail.com>
2022-09-03 13:11:23 +0100

    token

--- src/components/Login/spotify/Spotify.js
+++ src/components/Login/spotify/Spotify.js
@@ -11,6 +11,18 @@ const scopes = [
     "user-modify-playback-state",
 ];
 
+export const getTokenFromResponse = () => {
+    return window.location.hash
+        .substring(1)
+        .split("&")
+        .reduce((initial, item) => {
+            var parts = item.split("=");
+            initial[parts[0]] = decodeURIComponent(parts[1]);
+
+            return initial;
+        }, {});
+};
+
 export const loginUrl = `${authEndpoint}?client_id=${clientId}&redirect_uri=${redirectUri}&scope=${scopes.join(
     "%20"
 )}&response_type=token&show_dialog=true`;
---
+++ utils.md
@@ -0,0 +1,6 @@
+firebase:
+https://console.firebase.google.com/u/0/project/spotify-clone-2d3c4/overview
+
+spotify api from
+https://developer.spotify.com/dashboard/applications/1c86758e62a541f8975d0019dbb77698
+
